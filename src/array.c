87ef7af7ba845a86fdd98adff59a26a6bbeed7e2
Rayiusmen <1187210976@qq.com>
2022-06-28 14:10:17 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -6,13 +6,22 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
-  return 0;
-}
+  int lb = -1;
+    int ub = n;
+    while ( ub - lb > 1){
+      int x = (lb + ub ) / 2;
+      if(A[x] >=k ){
+         ub = x;
+      }
+      else {
+         lb = x;
+      }
+    }
+    printf("%d", ub);
+    return 0;
+  }
